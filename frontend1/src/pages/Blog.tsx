afd2d1773e1ea63370c14a8c5e8a75719150019c
HK <harshitakatariya1508@gmail.com>
2025-04-25 16:13:43 +0530

    fix issue in Blog.tsx

--- frontend1/src/pages/Blog.tsx
+++ frontend1/src/pages/Blog.tsx
@@ -5,22 +5,30 @@ import { Appbar } from "../components/Appbar";
 
 export const Blog = () => {
   const { id } = useParams();
-    const { loading, blog } = useBlog({
-      id : id || ""
-    });
-    
-    if (loading) {
-      return (
-        <div className="flex justify-center items-center min-h-screen text-lg font-medium">
-          Loading...
-          
-        </div>
-      );
-    }
+  const { loading, blog } = useBlog({
+    id: id || ""
+  });
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen text-lg font-medium">
+        Loading...
+      </div>
+    );
+  }
+
+  if (!blog) {
+    return (
+      <div className="flex justify-center items-center min-h-screen text-lg font-medium">
+        Blog not found
+      </div>
+    );
+  }
+
   return (
     <div>
       <Appbar />
-      <FullBlog blog={blog}/>
+      <FullBlog blog={blog} />
     </div>
-  )
-}
+  );
+};
